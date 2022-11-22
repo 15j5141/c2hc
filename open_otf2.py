d85093ad1fdb285ca42da85710fbf9f8dd002e37
hidden <j15j5j141@localhost.localdomain>
2022-11-22 15:12:19 +0900

    change

--- open_otf2.py
+++ open_otf2.py
@@ -14,16 +14,19 @@ diff_dic = {"_": [1]}
 diff_dic.clear()
 print(type(otf2))
 with otf2.reader.open('main.otf2.otf2') as trace:
+    # trace.definitions._set_clock_properties()
     print("Read {} string definitions".format(len(trace.definitions.strings)))
     print((trace.definitions.strings[1]))
     # for name in trace.definitions.strings:
     #     print("name:{}".format(name))
 
     for location, event in trace.events:
+        print(event.time, type(event).__name__, (event.metric.members[0].name if (type(event).__name__== "Metric") else "none"), sep=",")
         if type(event).__name__ == "Metric":
             event_name = event.metric.members[0].name
             time = event.time
             value = event.values[0]
+            # print(event_name,",")
             if (re.match("PAPI_.+", event_name)):
                 # エラー回避用
                 if not (event_name in diff_dic.keys()):
@@ -41,6 +44,7 @@ with otf2.reader.open('main.otf2.otf2') as trace:
                 if value != 0 and value!=1: # 最後にある謎の値を削除
                     value_dic[event_name].append(value)
                     diff_dic[event_name].append(diff_value)
+print("")
 
 # convert list to Series
 ###
