6f0f3a1f9dd19b83cf65a71a6004de2e95c5c903
hidden <29105288+15j5141@users.noreply.github.com>
2023-01-17 02:21:17 +0900

    解析用のプログラムを追加

--- testElasticModel.py
+++ testElasticModel.py
@@ -33,22 +33,24 @@ arr = np.array(df_mc)
 # arr = arr.T
 # X = arr.tolist()  # 説明変数（目的変数以外）
 
-scaler=StandardScaler()
+scaler_hc=StandardScaler()
+scaler_mc=StandardScaler()
 
 
-X = df_mc  # 説明変数（目的変数以外）
-scaler.fit(df_hc)
-df_hc = scaler.transform(df_hc)
-print(df_hc)
+scaler_hc.fit(df_hc)
+scaler_mc.fit(df_mc)
+df_hc = scaler_hc.transform(df_hc)
+X = scaler_mc.transform(df_mc)  # 説明変数（目的変数以外）
+# print(df_hc)
 for i in range(0,4):
     # df_hc = np.array(df_hc)  # 目的変数（住宅価格の中央値）
-    y = np.array(df_hc).reshape(-1,1)
+    # y = np.array(df_hc).reshape(-1,1)
     y = df_hc[:,i]
     # 学習・テストデータ分割
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
     # Elastic Net
-    model = ElasticNet(alpha=1.0, l1_ratio=0.5, max_iter=10000, tol=1e-4)
+    model = ElasticNet(alpha=1.0, l1_ratio=0.9, max_iter=10000, tol=1e-4)
     # model = SVR(kernel='linear', C=1, epsilon=0.1, gamma='auto')
     # model = RandomForestRegressor()
     # model = GradientBoostingRegressor()
@@ -57,8 +59,8 @@ for i in range(0,4):
     model.fit(X_train, y_train)
 
     print("score("+str(i)+")=" + str(model.score(X_test, y_test)))
-    # y_pred = elasticnet.predict(X_test)
-    # print("score=" + str(r2_score(y_test, y_pred)))
+    y_pred = model.predict(X_test)
+    print("score=" + str(r2_score(y_test, y_pred)))
     # pred_SVR = model.score(x_test, )
 
 # fig = plt.figure(figsize=(8, 6))
@@ -67,10 +69,10 @@ for i in range(0,4):
 # df_mc.plot()
 # df_hc.plot()
 # print(np.array(X_train).shape, np.array(y_train).shape, len(X_test), len(y_test))
-# plt.scatter(X_train, y_train)
-# plt.scatter(X_test, y_test)
-# 凡例の表示
-# plt.legend()
+plt.plot(X_test[:,50], y_pred, label="prediction",c="red")
+plt.scatter(X_test[:,50], y_test, label="actual")
+
+plt.legend()
 
 # プロット表示(設定の反映)
-# plt.show()
+plt.show()
---
+++ testLightGBM.py
@@ -0,0 +1,73 @@
+# ライブラリのインポート
+import numpy as np
+import pandas as pd
+import os
+import matplotlib
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# モデリング
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import lightgbm as lgb
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score  # モデル評価用(決定係数)
+
+# パラメータの準備
+params = {
+    "boosting_type": "gbdt",
+    "objective": "binary",
+    "metric": "auc",
+    "num_leaves": 16,
+    "learning_rate": 0.1,
+    "n_estimators": 100000,
+    "random_state": 0,
+}
+model = lgb.LGBMRegressor(**params)
+
+df_mc = pd.read_csv("out/mc_append.csv", header=None)
+df_hc = pd.read_csv("out/hc_append.csv", header=None)
+
+
+scaler_hc = StandardScaler()
+scaler_mc = StandardScaler()
+
+
+scaler_hc.fit(df_hc)
+scaler_mc.fit(df_mc)
+df_hc = scaler_hc.transform(df_hc)
+X = scaler_mc.transform(df_mc)  # 説明変数（目的変数以外）
+y = df_hc[:, 3]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+model.fit(
+    X_train,
+    y_train,
+    eval_set=[(X_train, y_train), (X_test, y_test)],
+    early_stopping_rounds=100,
+)
+print(model.best_iteration_)
+y_pred = model.predict(X_test)
+
+print(r2_score(y_test, y_pred))
+fig = plt.figure()
+ax = fig.add_subplot()
+
+mean_mc, scale_mc = scaler_mc.mean_, scaler_mc.scale_
+mean_hc, scale_hc = scaler_hc.mean_, scaler_hc.scale_
+# y_pred*scale_hc + mean_hc
+ax.scatter(X_test[:, 50], y_pred, c=[(1, 0, 0)], label="prediction")
+for i in range(1, X_test.shape[0]):
+    ax.scatter(X_test[:, i], y_pred, c=[(0.5 + (3 * i / 255), 0, 0)])
+ax.set_xlabel("mnemonic count")
+ax.set_ylabel("hardware count")
+ax.set_xlim(-1, 2)
+ax.set_ylim(-1, 1.2)
+
+ax.scatter(X_test[:, 50], y_test, c=[(0, 0, 1)], label="actual")
+for i in range(0, X_test.shape[0]):
+    ax.scatter(X_test[:, i], y_test, c=[(0, 0, 0.5 + (3 * i / 255))])
+
+ax.legend()
+fig.show()
+plt.show()
