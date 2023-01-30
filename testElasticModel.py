2740d7825cae0b6193b954acca78d67e6a458017
hidden <29105288+15j5141@users.noreply.github.com>
2023-01-30 20:08:39 +0900

    グラフの表現を変更

--- testElasticModel.py
+++ testElasticModel.py
@@ -43,13 +43,15 @@ scaler_mc.fit(df_mc)
 df_hc = scaler_hc.transform(df_hc)
 X = scaler_mc.transform(df_mc)  # 説明変数（目的変数以外）
 # print(df_hc)
+y_pred_list = []
+y_test_list = []
 for i in range(0, 4):
     # df_hc = np.array(df_hc)  # 目的変数（住宅価格の中央値）
     # y = np.array(df_hc).reshape(-1,1)
     y = df_hc[:, i]
     # 学習・テストデータ分割
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=0
+        X, y, test_size=0.2, random_state=0
     )
 
     # Elastic Net
@@ -63,6 +65,8 @@ for i in range(0, 4):
 
     print("score(" + str(i) + ")=" + str(model.score(X_test, y_test)))
     y_pred = model.predict(X_test)
+    y_pred_list.append(y_pred)
+    y_test_list.append(y_test)
     print("score=" + str(r2_score(y_test, y_pred)))
     # pred_SVR = model.score(x_test, )
 
@@ -72,9 +76,38 @@ for i in range(0, 4):
 # df_mc.plot()
 # df_hc.plot()
 # print(np.array(X_train).shape, np.array(y_train).shape, len(X_test), len(y_test))
-plt.plot(X_test[:, 50], y_pred, label="prediction", c="red")
-plt.scatter(X_test[:, 50], y_test, label="actual")
+# plt.plot(X_test[:, 50], y_pred, label="prediction", c="red")
+# plt.scatter(X_test[:, 50], y_test, label="actual")
 
+fig = plt.figure()
+ax = fig.add_subplot()
+width=0.35
+ax.set_xlabel("program")
+ax.set_ylabel("hardware count")
+mean_mc, scale_mc = scaler_mc.mean_, scaler_mc.scale_
+mean_hc, scale_hc = scaler_hc.mean_, scaler_hc.scale_
+# y_pred*scale_hc + mean_hc
+unscaled_hc_pred = (y_pred_list[3] * scale_hc[3] + mean_hc[3])
+unscaled_hc_acc = (y_test_list[3] * scale_hc[3] + mean_hc[3])
+ax.bar(
+    [(i-width/2) for i in range(1, len(X_test) + 1)],
+    unscaled_hc_pred,
+    width=width,
+    color=[(0, 1, 0, 0.2)], 
+    label="prediction")
+ax.bar(
+    [(i+width/2) for i in range(1, len(y_test) + 1)],
+    unscaled_hc_acc,
+    width=width,
+    color=[(0, 0, 1, 0.2)],
+    label="actual",
+)
+ax.bar(
+    [(i) for i in range(1, len(y_test) + 1)],
+    np.abs(unscaled_hc_acc - unscaled_hc_pred),
+    width=width,
+    color=[(1, 0, 0)], 
+    label="error")
 plt.legend()
 
 # プロット表示(設定の反映)
--- testLightGBM.py
+++ testLightGBM.py
@@ -4,7 +4,7 @@ import matplotlib
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, r2_score
+from sklearn.metrics import accuracy_score, confusion_matrix, r2_score, mean_squared_error
 from sklearn.preprocessing import StandardScaler
 import lightgbm as lgb
 
@@ -34,8 +34,13 @@ X = scaler_mc.transform(df_mc)
 y = df_hc[:, 3]
 
 # 学習と評価
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-model.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+model.fit(
+    X_train,
+    y_train,
+    eval_set=[(X_train, y_train), (X_test, y_test)],
+    early_stopping_rounds=100,
+)
 
 print(model.best_iteration_)
 y_pred = model.predict(X_test)
@@ -45,24 +50,45 @@ print(r2_score(y_test, y_pred))
 # 可視化
 fig = plt.figure()
 ax = fig.add_subplot()
+width=0.35
 
 mean_mc, scale_mc = scaler_mc.mean_, scaler_mc.scale_
 mean_hc, scale_hc = scaler_hc.mean_, scaler_hc.scale_
 
 # y_pred*scale_hc + mean_hc
-ax.scatter(X_test[:, 50], y_pred, c=[(1, 0, 0)], label="prediction")
-for i in range(1, X_test.shape[0]):
-    ax.scatter(X_test[:, i], y_pred, c=[(0.5 + (3 * i / 255), 0, 0)])
+unscaled_hc_pred = (y_pred * scale_hc[3] + mean_hc[3])
+unscaled_hc_acc = (y_test * scale_hc[3] + mean_hc[3])
+ax.bar(
+    [(i-width/2) for i in range(1, len(X_test) + 1)],
+    unscaled_hc_pred,
+    width=width,
+    color=[(0, 1, 0, 0.2)], 
+    label="prediction")
+# for i in range(1, X_test.shape[0]):
+#     ax.scatter(X_test[:, i], y_pred, c=[(0.5 + (3 * i / 255), 0, 0)])
 
-ax.set_xlabel("mnemonic count")
+ax.set_xlabel("program")
 ax.set_ylabel("hardware count")
-ax.set_xlim(-1, 2)
-ax.set_ylim(-1, 1.2)
+# ax.set_xlim(-1, 4)
+# ax.set_ylim(-1, 1.2)
 
-ax.scatter(X_test[:, 50], y_test, c=[(0, 0, 1)], label="actual")
-for i in range(0, X_test.shape[0]):
-    ax.scatter(X_test[:, i], y_test, c=[(0, 0, 0.5 + (3 * i / 255))])
-
+ax.bar(
+    [(i+width/2) for i in range(1, len(y_test) + 1)],
+    unscaled_hc_acc,
+    width=width,
+    color=[(0, 0, 1, 0.2)],
+    label="actual",
+)
+ax.bar(
+    [(i) for i in range(1, len(y_test) + 1)],
+    np.abs(unscaled_hc_acc - unscaled_hc_pred),
+    width=width,
+    color=[(1, 0, 0)], 
+    label="error")
+# for i in range(0, X_test.shape[0]):
+#     ax.scatter(X_test[:, i], y_test, c=[(0, 0, 0.5 + (3 * i / 255))])
+print(X_test[:, 50])
 ax.legend()
 fig.show()
 plt.show()
+
